e6e399b2da5679e8d232d4544421152cc1a221f0
Aurume <51314673+Aurume@users.noreply.github.com>
2023-01-11 21:46:27 +0200

    užklausos

---
+++ uzduotys_sql4dalis_0111.sql
@@ -0,0 +1,58 @@
+--2.
+--Klientų sukūrimas
+
+--INSERT INTO "customer" ("f_name", "l_name", "email")
+--VALUES ("Ona", "Onaitė", "one@gmail.com")
+
+--INSERT INTO "customer" ("f_name", "l_name", "email")
+--VALUES ("Jonas", "Jonaitis", "jonis@gmail.com")
+
+--INSERT INTO "customer" ("f_name", "l_name", "email")
+--VALUES ("Jurga", "Jurgytė", "jurge@gmail.com")
+
+--INSERT INTO "customer" ("f_name", "l_name", "email")
+--VALUES ("Ilona", "Varnauskė", "iva@gmail.com")
+
+--INSERT INTO "customer" ("f_name", "l_name", "email")
+--VALUES ("Jokūbas", "Bernauskas", "jober@gmail.com")
+
+--statuso lentelės papildymas
+
+--INSERT INTO "status" ("name")
+--VALUES ("patvirtintas")
+
+--INSERT INTO "status" ("name")
+--VALUES ("vykdomas")
+
+--INSERT INTO "status" ("name")
+--VALUES ("įvykdytas")
+
+--INSERT INTO "status" ("name")
+--VALUES ("atmestas")
+
+--produktų įrašymas
+--INSERT INTO "product" ("name", "price")
+--VALUES ("Stalas", "180")
+
+--INSERT INTO "product" ("name", "price")
+--VALUES ("Kėdė", "50")
+
+--INSERT INTO "product" ("name", "price")
+--VALUES ("Lempa", "120")
+
+--INSERT INTO "product" ("name", "price")
+--VALUES ("Kompiuteris", "820")
+
+--INSERT INTO "product" ("name", "price")
+--VALUES ("Ausinės", "220")
+
+--INSERT INTO "product" ("name", "price")
+--VALUES ("Tušinukas", "2")
+
+--INSERT INTO "product" ("name", "price")
+--VALUES ("Kalendorius", "12")
+
+--TRŪKSTA Dar užsakymų, ir jų pozicijų.
+
+
+
